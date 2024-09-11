76e4493956a6a9257d8ca066c93b645bb2e88fac
noah.diderich <noah.diderich@student.kdg.be>
2024-09-11 23:16:43 +0200

    changing id to uuid and updating month-number converter

--- db/queries.py
+++ db/queries.py
@@ -1,6 +1,6 @@
 create_table_query = """
 CREATE TABLE IF NOT EXISTS movies (
-  id INT AUTO_INCREMENT PRIMARY KEY,
+  uuid char(36) not null PRIMARY KEY,
   title VARCHAR(255) NOT NULL,
   director VARCHAR(255) NOT NULL,
   category VARCHAR(255) NOT NULL,
@@ -26,7 +26,7 @@ ALTER TABLE movies MODIFY description TEXT NOT NULL
 """
 
 insert_query = """
-INSERT INTO movies (title, director, category, description, cinema, date_time, image_url, info_link, ticket_link)
-VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+INSERT INTO movies (uuid, title, director, category, description, cinema, date_time, image_url, info_link, ticket_link)
+VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
 """
 
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from db.database import run_query, run_insert_query
 from db.queries import *
 from scraping.scrape_movie_agendas import scrape_movie_agendas
+import uuid
 
 # empty the table
 run_query(empty_table_query)
@@ -11,7 +12,7 @@ movies = scrape_movie_agendas()
 # insert the data
 #'''
 for index, row in movies.iterrows():
-    run_insert_query(insert_query, (row['name'], row['director'], row['category'], row['description'],
+    run_insert_query(insert_query, (str(uuid.uuid4()), row['name'], row['director'], row['category'], row['description'],
                                     row['cinema'], row['date_time'], row['image_url'], row['info_link'],
                                     row['ticket_link']))
 print("Data inserted successfully!")
